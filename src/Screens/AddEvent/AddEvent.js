680df169f1d94136248e0cd67ce5967576c50019
Lee Hyun Se <0409hshs@gmail.com>
2020-11-26 01:37:41 +0900

    handleCheckBox
    Update handleCheckBox
    but not complete
    isChecked state need error handling

--- src/Screens/AddEvent/AddEvent.js
+++ src/Screens/AddEvent/AddEvent.js
@@ -1,57 +1,81 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { DatePicker } from "antd";
 import moment from "moment";
 import MainHeader from "../../Components/MainHeader";
 import Helmet from "react-helmet";
-import { AddEventBackground, AddEventModal, AddEventTitle,
-  Form, FormItem, FormLabel, FormTextInput, CheckboxInput, CheckboxLabelDefault, CheckBoxUl,
-  CancelButton, SubmitButton, ButtonWrapper, SearchBox, SearchInput } from "./AddEventStyle";
+import {
+  AddEventBackground,
+  AddEventModal,
+  AddEventTitle,
+  Form,
+  FormItem,
+  FormLabel,
+  FormTextInput,
+  CheckboxInput,
+  CheckboxLabelDefault,
+  CheckBoxUl,
+  CancelButton,
+  SubmitButton,
+  ButtonWrapper,
+  SearchBox,
+  SearchInput,
+} from "./AddEventStyle";
 import { useHistory } from "react-router-dom";
 import SearchImg from "../../assets/img/SearchImg";
 import ParticipantsList from "./ParticipantsList";
 import SearchList from "./SearchList";
 
-
 const AddEvent = () => {
   const history = useHistory();
-  
+
   const [title, setTitle] = useState("");
   const [selectedDataList, setSelectedDataList] = useState([]);
   const [startdate, setStartdate] = useState(new Date());
   const [enddate, setEnddate] = useState(new Date());
   const [keyword, setKeyword] = useState("");
   const [participants, setParticipants] = useState([
-    "고서영", "최서희", "사연진", "최승연", "이현세", "김지민", "박지호",
-    "고서영", "최서희", "사연진", "최승연", "이현세", "김지민", "박지호",
+    "고서영",
+    "최서희",
+    "사연진",
+    "최승연",
+    "이현세",
+    "김지민",
+    "박지호",
+    "고서영",
+    "최서희",
+    "사연진",
+    "최승연",
+    "이현세",
+    "김지민",
+    "박지호",
   ]);
   const [memo, setMemo] = useState("");
-
+  const [isChecked, setIsChecked] = useState(true);
   const [dataInfo, setDataInfo] = useState([
-    {id: 1, name: "step", value:"걸음 수", isChecked: false},
-    {id: 2, name: "waist", value: "허리둘레", isChecked: false},
-    {id: 3, name: "calories", value: "소모칼로리", isChecked: false},
-    {id: 4, name: "distance", value: "걸음거리", isChecked: false},
-    {id: 5, name: "gaitSpeed", value: "걸음속도", isChecked: false},
+    { id: 1, name: "step", value: "걸음 수", isChecked },
+    { id: 2, name: "waist", value: "허리둘레", isChecked },
+    { id: 3, name: "calories", value: "소모칼로리", isChecked },
+    { id: 4, name: "distance", value: "걸음거리", isChecked },
+    { id: 5, name: "gaitSpeed", value: "걸음속도", isChecked },
   ]);
 
   useEffect(() => {
     setDataInfo(dataInfo);
-    if(keyword == ""){
+    if (keyword == "") {
       //전체 보여주기
-    }else{
+    } else {
       // 검색
       console.log(keyword + " 검색");
     }
-    return () => {
-    }
-  }, [keyword, dataInfo])
+    return () => {};
+  }, [keyword, dataInfo]);
 
   const onChange = (event) => {
     const {
-      target: { value },
+      target: { value, name },
     } = event;
 
-    switch (event.target.name) {
+    switch (name) {
       case "title":
         setTitle(value);
         break;
@@ -67,91 +91,104 @@ const AddEvent = () => {
   };
 
   const onChangeDate = (event, name) => {
-    // if (typeof event !== Object) return; // 이거하면 날짜 안바뀜..
+    if (!event && typeof event !== "object") return;
 
     const { _d } = event;
-    
-    console.log("event name: "+name);
-    if(name === "startDate"){
+
+    console.log("event name: " + name);
+    if (name === "startDate") {
       setStartdate(_d);
-      console.log("startdate: "+startdate);
-    }else if(name === "endDate"){
+      console.log("startdate: " + startdate);
+    } else if (name === "endDate") {
       setEnddate(_d);
-      console.log("enddate: "+enddate);
+      console.log("enddate: " + enddate);
     }
-    
   };
 
-  const onCancelBtn = ()=>{
+  const onCancelBtn = () => {
     history.goBack();
-  }
+  };
 
   const onSubmitBtn = () => {
     let selectedList = isSelected();
 
     var str =
-      "이벤트명: "+title+"\n"+
-      "사용데이터: (순서대로)" +selectedList+"\n"+
-      "기간1: "+startdate+"\n"+
-      "기간2: "+enddate+"\n"+
-      "참가자리스트: "+"\n"+
-      "메모: "+memo;
+      "이벤트명: " +
+      title +
+      "\n" +
+      "사용데이터: (순서대로)" +
+      selectedList +
+      "\n" +
+      "기간1: " +
+      startdate +
+      "\n" +
+      "기간2: " +
+      enddate +
+      "\n" +
+      "참가자리스트: " +
+      "\n" +
+      "메모: " +
+      memo;
 
     console.log(str);
 
-    if(title === ""){
+    if (title === "") {
       alert("이벤트명을 입력해주세요.");
-    }else if(selectedList.length <= 0){
+    } else if (selectedList.length <= 0) {
       alert("사용데이터를 선택해주세요.");
-    }else if(startdate === null || enddate === null){
+    } else if (startdate === null || enddate === null) {
       alert("기간을 선택해주세요.");
-    }else{
-      alert(str+"등록 완료");
+    } else {
+      alert(str + "등록 완료");
     }
     // 빈 참가리스트 예외처리
 
     // history.goBack()
-  }
+  };
 
   const isSelected = () => {
-    var selectedList = []
-    dataInfo.forEach(data => {
-      if(data.isChecked === true){
+    var selectedList = [];
+    dataInfo.forEach((data) => {
+      if (data.isChecked === true) {
         selectedList.push(data.name);
       }
-    })
+    });
     console.log(selectedList);
     return selectedList;
-  }
+  };
 
-  const CheckBox = props => {
-      return (
-        <li>
-        <CheckboxInput
-         key={props.id} 
-         type="checkbox" 
-         checked={props.isChecked}
-         value={props.value} 
-         id={props.name}
-         onClick={props.handleCheckChieldElement}
-        /> 
-        <CheckboxLabelDefault 
-          for={props.name}
-          isChecked={props.isChecked}>
-            {props.value}
+  const CheckBox = (props) => {
+    return (
+      <li>
+        <CheckboxLabelDefault htmlFor={props.name} isChecked={props.isChecked}>
+          {props.value}
         </CheckboxLabelDefault>
-        </li>
+        <CheckboxInput
+          name={props.name}
+          key={props.id}
+          type="checkbox"
+          checked={isChecked}
+          value={props.value}
+          id={props.name}
+          onChange={props.handleCheckbox}
+        />
+      </li>
+    );
+  };
+
+  const handleCheckbox = (event) => {
+    let {
+      target: { value, checked },
+    } = event;
+
+    setIsChecked(checked);
+
+    setDataInfo((prevState) =>
+      prevState.map((data) =>
+        data.value === value ? { ...data, isChecked: checked } : data
       )
-  }
-
-  const handleCheckChieldElement = (event) => {
-    console.log(event.target.value);
-    console.log(event.target.checked);
-
-    dataInfo.forEach(data => {
-       if (data.value === event.target.value) data.isChecked = event.target.checked
-    });
-  }
+    );
+  };
 
   const onFocus = (e) => {
     // e.preventDefault();
@@ -159,8 +196,7 @@ const AddEvent = () => {
     // if(name === "keyword"){
     //   console.log("keyword focus");
     // }
-  }
-
+  };
 
   return (
     <>
@@ -173,54 +209,76 @@ const AddEvent = () => {
           <Form>
             <FormItem>
               <FormLabel>이벤트 명</FormLabel>
-              <FormTextInput type="text" name="title" value={title} onChange={onChange} placeholder="이벤트 명을 입력해주세요."/>
+              <FormTextInput
+                type="text"
+                name="title"
+                value={title}
+                onChange={onChange}
+                placeholder="이벤트 명을 입력해주세요."
+              />
             </FormItem>
             <FormItem>
               <FormLabel>사용 데이터</FormLabel>
               <CheckBoxUl>
-              {
-                dataInfo.map((data) => {
-                  return (<CheckBox handleCheckChieldElement={handleCheckChieldElement} {...data} />)
-                })
-              }
+                {dataInfo.map((data) => {
+                  return (
+                    <CheckBox
+                      key={data.id}
+                      handleCheckbox={handleCheckbox}
+                      {...data}
+                    />
+                  );
+                })}
               </CheckBoxUl>
             </FormItem>
             <FormItem>
               <FormLabel>기간</FormLabel>
               <DatePicker
                 selected={startdate}
-                onChange={date => onChangeDate(date, 'startDate')}
+                onChange={(date) => onChangeDate(date, "startDate")}
                 bordered={false}
                 defaultValue={moment()}
-              />
+              ></DatePicker>
               <DatePicker
                 selected={enddate}
-                onChange={date => onChangeDate(date, 'endDate')}
+                onChange={(date) => onChangeDate(date, "endDate")}
                 bordered={false}
                 defaultValue={moment()}
-              />
+              ></DatePicker>
             </FormItem>
             <FormItem>
               <FormLabel>참가자</FormLabel>
-              <div style={{width:"60%"}}>
+              <div style={{ width: "60%" }}>
                 <SearchBox>
-                  <SearchInput type="text" name="keyword" value={keyword} onChange={onChange} onFocus={onFocus}/>
-                  <SearchImg/>
+                  <SearchInput
+                    type="text"
+                    name="keyword"
+                    value={keyword}
+                    onChange={onChange}
+                    onFocus={onFocus}
+                  />
+                  <SearchImg />
                 </SearchBox>
                 <div>
-                  <SearchList visibility={true}/>
-                  <ParticipantsList list={participants}/> 
+                  <SearchList visibility={true} />
+                  <ParticipantsList list={participants} />
                 </div>
               </div>
             </FormItem>
             <FormItem>
               <FormLabel>메모</FormLabel>
-              <FormTextInput type="text" name="memo" value={memo} onChange={onChange} placeholder="메모를 입력해주세요."/>
+              <FormTextInput
+                type="text"
+                name="memo"
+                value={memo}
+                onChange={onChange}
+                placeholder="메모를 입력해주세요."
+              ></FormTextInput>
             </FormItem>
           </Form>
           <ButtonWrapper>
             <CancelButton onClick={onCancelBtn}>취소하기</CancelButton>
-            <SubmitButton onClick={onSubmitBtn}>등록하기</SubmitButton>
+            <SubmitButton onSubmit={onSubmitBtn}>등록하기</SubmitButton>
           </ButtonWrapper>
         </AddEventModal>
       </AddEventBackground>
--- src/Screens/AddEvent/AddEventStyle.js
+++ src/Screens/AddEvent/AddEventStyle.js
@@ -2,129 +2,129 @@ import Search from "antd/lib/input/Search";
 import styled from "styled-components";
 
 export const AddEventBackground = styled.div`
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: rgba(0, 0, 0, 0.6);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.6);
 `;
 
 export const AddEventModal = styled.div`
-    width: 66%;
-    height: 76%;
-    background-color: white;
-    position: relative;
-    box-sizing: border-box;
-    margin: 120px auto;
-    padding: 20px;
-    border-radius: 30px;
-    background: #fff; 
+  width: 66%;
+  height: 76%;
+  background-color: white;
+  position: relative;
+  box-sizing: border-box;
+  margin: 120px auto;
+  padding: 20px;
+  border-radius: 30px;
+  background: #fff;
 `;
 
 export const AddEventTitle = styled.h1`
-    font-size: 24px;
-    margin: 40px auto 50px auto;
-    text-align: center;
-    color: #707070;
+  font-size: 24px;
+  margin: 40px auto 50px auto;
+  text-align: center;
+  color: #707070;
 `;
 
 export const Form = styled.form`
-    display: flex;
-    flex-wrap: wrap;
-    text-align: left;
-    font-size: 18px;
-    flex-direction: column;
-    width: 80%;
-    margin: auto;
+  display: flex;
+  flex-wrap: wrap;
+  text-align: left;
+  font-size: 18px;
+  flex-direction: column;
+  width: 80%;
+  margin: auto;
 `;
 
 export const FormItem = styled.div`
-    margin: 10px;
-    display: flex;
+  margin: 10px;
+  display: flex;
 `;
 
-export const FormLabel = styled.label`
-    width: 15%;
-    font-size: 18px;
-    color: #707070;
-    text-algin: left;
-    margin-right: 15px;
-    height: 30px;
-    line-height: 30px;
-`; 
-
-export const FormTextInput =  styled.input`
-    width: 80%;
-    border: none;
-    border-bottom: 1px solid #000000;
+export const FormLabel = styled.span`
+  width: 15%;
+  font-size: 18px;
+  color: #707070;
+  text-algin: left;
+  margin-right: 15px;
+  height: 30px;
+  line-height: 30px;
 `;
 
-export const CheckboxInput = styled.input.attrs({type: 'checkbox'})`
-    display: none;
+export const FormTextInput = styled.input`
+  width: 80%;
+  border: none;
+  border-bottom: 1px solid #000000;
 `;
 
-export const CheckboxLabelDefault = styled.label.attrs({ type: 'checkbox' })`
-    border: 1px solid #CCCCCC;
-    border-radius: 16px;
-    text-align: center;
-    margin: auto 10px;
-    display: inline-block;
-    padding: 10px 20px;
-    font-size: 16px;
-    background-color: ${props => props.isChecked ? '#4F42A7' : '#fff'};
-    color: ${props => props.isChecked ? '#fff' : '#707070'};
+export const CheckboxInput = styled.input.attrs({ type: "checkbox" })`
+  display: none;
+`;
 
-    &:checked::before {
-        background-color: '#4F42A7';
-    }
+export const CheckboxLabelDefault = styled.label`
+  border: 1px solid #cccccc;
+  border-radius: 16px;
+  text-align: center;
+  margin: auto 10px;
+  display: inline-block;
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: ${(props) => (props.isChecked ? "#fff" : "#4F42A7")};
+  color: ${(props) => (props.isChecked ? "#707070" : "#fff")};
+
+  &:checked::before {
+    background-color: "#4F42A7";
+  }
 `;
 
 export const CheckBoxUl = styled.ul`
-    display: flex;
+  display: flex;
 `;
 
 export const CancelButton = styled.p`
-    padding: 10px 20px;
-    font-size: 16px;
-    background-color: #fff;
-    color: #707070;
-    border: 1px solid #CCCCCC;
-    border-radius: 16px;
-    display: inline-block;
-    margin: 5px;
-`
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #fff;
+  color: #707070;
+  border: 1px solid #cccccc;
+  border-radius: 16px;
+  display: inline-block;
+  margin: 5px;
+`;
 export const SubmitButton = styled.p`
-    padding: 10px 20px;
-    font-size: 16px;
-    background-color: #4F42A7;
-    color: #fff;
-    border: 1px solid #CCCCCC;
-    border-radius: 16px;
-    display: inline-block;
-    margin: 5px;
-`
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #4f42a7;
+  color: #fff;
+  border: 1px solid #cccccc;
+  border-radius: 16px;
+  display: inline-block;
+  margin: 5px;
+`;
 
 export const ButtonWrapper = styled.div`
-    text-align: center;
-    margin-top: 50px;
-    position: absolute;
-    bottom: 44px;
-    right: 0;
-    left: 0;
-`
+  text-align: center;
+  margin-top: 50px;
+  position: absolute;
+  bottom: 44px;
+  right: 0;
+  left: 0;
+`;
 
 export const SearchBox = styled.div`
-    border: 1px solid #CCCCCC;
-    border-radius: 6px;
-    width: 100%;
-    height: 40px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-`
+  border: 1px solid #cccccc;
+  border-radius: 6px;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
 
 export const SearchInput = styled.input`
-    width: 80%;
-    border: none;
-`
+  width: 80%;
+  border: none;
+`;
