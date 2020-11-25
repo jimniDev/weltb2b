daec1c857c6b7fa867763ab10628929533981235
syoung125 <laura120555@gmail.com>
2020-11-25 18:30:41 +0900

    Add Event front 구현 중
    check box 동작 에러

--- src/Screens/AddEvent/AddEvent.js
+++ src/Screens/AddEvent/AddEvent.js
@@ -1,15 +1,167 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { DatePicker } from "antd";
+import moment from "moment";
 import MainHeader from "../../Components/MainHeader";
 import Helmet from "react-helmet";
+import { AddEventBackground, AddEventModal, AddEventTitle,
+  Form, FormItem, FormLabel, FormTextInput, CheckboxInput, CheckboxLabelDefault, CheckBoxUl,
+  CancelButton, SubmitButton, ButtonWrapper } from "./AddEventStyle";
+import { useHistory } from "react-router-dom";
+
 
 const AddEvent = () => {
+  const history = useHistory();
+  
+  const [title, setTitle] = useState("");
+  const [selectedDataList, setSelectedDataList] = useState([]);
+  const [startdate, setStartdate] = useState(new Date());
+  const [enddate, setEnddate] = useState(new Date());
+  const [participants, setParticipants] = useState([]);
+  const [memo, setMemo] = useState("");
+
+  const [dataInfo, setDataInfo] = useState([
+    {id: 1, name: "step", value:"걸음 수", isChecked: false},
+    {id: 2, name: "waist", value: "허리둘레", isChecked: false},
+    {id: 3, name: "calories", value: "소모칼로리", isChecked: true},
+    {id: 4, name: "distance", value: "걸음거리", isChecked: false},
+    {id: 5, name: "gaitSpeed", value: "걸음속도", isChecked: false},
+  ]);
+
+  useEffect(() => {
+    return () => {
+    }
+  }, [dataInfo])
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    switch (event.target.name) {
+      case "title":
+        setTitle(value);
+        break;
+      case "memo":
+        setMemo(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const onChangeDate = (event, name) => {
+    // if (typeof event !== Object) return; // 이거하면 날짜 안바뀜..
+
+    const { _d } = event;
+    
+    console.log("event name: "+name);
+    if(name === "startDate"){
+      setStartdate(_d);
+      console.log("startdate: "+startdate);
+    }else if(name === "endDate"){
+      setEnddate(_d);
+      console.log("enddate: "+enddate);
+    }
+    
+  };
+
+  const onCancelBtn = ()=>{
+    history.goBack();
+  }
+
+  const onSubmitBtn = () => {
+    console.log(
+      "이벤트명: "+title,
+      "사용데이터: (순서대로)" +JSON.stringify(dataInfo),
+      "기간1: "+startdate, 
+      "기간2: "+enddate,
+      "참가자리스트: ",
+      "메모: "+memo
+    )
+    // history.goBack()
+  }
+
+  const CheckBox = props => {
+      return (
+        <li>
+        <CheckboxInput
+         key={props.id} 
+         type="checkbox" 
+         checked={props.isChecked}
+         value={props.value} 
+         name={props.name}
+        /> 
+        <CheckboxLabelDefault 
+          onClick={props.handleCheckChieldElement}
+          isChecked={props.isChecked}>
+            {props.value}
+        </CheckboxLabelDefault>
+        </li>
+      )
+  }
+
+  const handleCheckChieldElement = (event) => {
+    let _dataInfo = dataInfo;
+    _dataInfo.forEach(data => {
+       if (data.value === event.target.value) data.isChecked = event.target.checked
+    });
+    setDataInfo(_dataInfo);
+  }
+
   return (
     <>
       <Helmet>
         <title>AddEvent | WELT</title>
       </Helmet>
+      <AddEventBackground>
+        <AddEventModal>
+          <AddEventTitle>이벤트추가</AddEventTitle>
+          <Form>
+            <FormItem>
+              <FormLabel>이벤트 명</FormLabel>
+              <FormTextInput type="text" name="title" value={title} onChange={onChange} placeholder="이벤트 명을 입력해주세요."/>
+            </FormItem>
+            <FormItem>
+              <FormLabel>사용 데이터</FormLabel>
+              <CheckBoxUl>
+              {
+                dataInfo.map((data) => {
+                  return (<CheckBox handleCheckChieldElement={handleCheckChieldElement} {...data} />)
+                })
+              }
+              </CheckBoxUl>
+            </FormItem>
+            <FormItem>
+              <FormLabel>기간</FormLabel>
+              <DatePicker
+                selected={startdate}
+                onChange={date => onChangeDate(date, 'startDate')}
+                bordered={false}
+                defaultValue={moment()}
+              />
+              <DatePicker
+                selected={enddate}
+                onChange={date => onChangeDate(date, 'endDate')}
+                bordered={false}
+                defaultValue={moment()}
+              />
+            </FormItem>
+            <FormItem>
+              <FormLabel>참가자</FormLabel>
+              <input type="text" name="participant" />
+            </FormItem>
+            <FormItem>
+              <FormLabel>메모</FormLabel>
+              <FormTextInput type="text" name="memo" value={memo} onChange={onChange} placeholder="메모를 입력해주세요."/>
+            </FormItem>
+          </Form>
+          <ButtonWrapper>
+            <CancelButton onClick={onCancelBtn}>취소하기</CancelButton>
+            <SubmitButton onClick={onSubmitBtn}>등록하기</SubmitButton>
+          </ButtonWrapper>
+        </AddEventModal>
+      </AddEventBackground>
       <MainHeader />
-      AddEvent
     </>
   );
 };
---
+++ src/Screens/AddEvent/AddEventStyle.js
@@ -0,0 +1,113 @@
+import styled from "styled-components";
+
+export const AddEventBackground = styled.div`
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.6);
+`;
+
+export const AddEventModal = styled.div`
+    width: 66%;
+    height: 76%;
+    background-color: white;
+    position: relative;
+    box-sizing: border-box;
+    margin: 120px auto;
+    padding: 20px;
+    border-radius: 30px;
+    background: #fff; 
+`;
+
+export const AddEventTitle = styled.h1`
+    font-size: 24px;
+    margin: 40px auto 50px auto;
+    text-align: center;
+    color: #707070;
+`;
+
+export const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+    text-align: left;
+    font-size: 18px;
+    flex-direction: column;
+    width: 80%;
+    margin: auto;
+`;
+
+export const FormItem = styled.div`
+    margin: 10px;
+    display: flex;
+`;
+
+export const FormLabel = styled.label`
+    width: 15%;
+    font-size: 18px;
+    color: #707070;
+    text-algin: left;
+    margin-right: 15px;
+    height: 30px;
+    line-height: 30px;
+`; 
+
+export const FormTextInput =  styled.input`
+    width: 80%;
+    border: none;
+    border-bottom: 1px solid #000000;
+`;
+
+export const CheckboxInput = styled.input.attrs({type: 'checkbox'})`
+    opacity: 0;
+    position: fixed;
+    width: 0;
+`;
+
+export const CheckboxLabelDefault = styled.label.attrs({ type: 'checkbox' })`
+    border: 1px solid #CCCCCC;
+    border-radius: 16px;
+    text-align: center;
+    margin: auto 10px;
+    display: inline-block;
+    padding: 10px 20px;
+    font-size: 16px;
+    background-color: ${props => props.isChecked ? '#4F42A7' : '#fff'};
+    color: ${props => props.isChecked ? '#fff' : '#707070'};
+
+    &:checked::before {
+        background-color: '#4F42A7';
+    }
+`;
+
+export const CheckBoxUl = styled.ul`
+    display: flex;
+`;
+
+export const CancelButton = styled.p`
+    padding: 10px 20px;
+    font-size: 16px;
+    background-color: #fff;
+    color: #707070;
+    border: 1px solid #CCCCCC;
+    border-radius: 16px;
+    display: inline-block;
+    margin: 5px;
+`
+export const SubmitButton = styled.p`
+    padding: 10px 20px;
+    font-size: 16px;
+    background-color: #4F42A7;
+    color: #fff;
+    border: 1px solid #CCCCCC;
+    border-radius: 16px;
+    display: inline-block;
+    margin: 5px;
+`
+
+export const ButtonWrapper = styled.div`
+    text-align: center;
+    margin-top: 50px;
+`
+// https://stackoverflow.com/questions/56655066/using-pseudo-code-to-style-radio-button-with-styled-components
