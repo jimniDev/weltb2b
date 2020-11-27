026897eca45581999a1803644833bcc44dff3653
kds3318 <56024942+kds3318@users.noreply.github.com>
2020-11-27 09:01:54 +0900

    Update LineChart.js

--- src/Components/LineChart.js
+++ src/Components/LineChart.js
@@ -5,24 +5,46 @@ import JSON from "../data.json";
 
 const mountNode = document.getElementById("root");
 
-const LineChart = ({ count }) => {
-  const { data } = JSON;
-  return (
-    <Chart
-      scale={{ count: { min: 0, max: 25000 } }}
-      padding={[30, 0, 70, 40]}
-      autoFit
-      height={320}
-      data={data}
-      interactions={["element-active"]}
-    >
-      <Legend position={"top-left"}></Legend>
-      <Line shape="line" position="month*count" color="year" label={""} />
-      <Point position="month*count" color="year" />
-      <Tooltip shared showCrosshairs />
-    </Chart>
-  );
+const LineChart = ({ count, data, num }) => {
+  //const { data } = JSON;
+
+  //console.log(data);
+  if(num == 1){
+    return (
+      <Chart
+        scale={{ count: { min: 0, max: 25000 } }}
+        padding={[30, 0, 70, 40]}
+        autoFit
+        height={320}
+        data={data}
+        interactions={["element-active"]}
+      >
+        <Legend position={"top-left"}></Legend>
+        <Line shape="line" position="month*count" color="year" label={""} />
+        <Point position="month*count" color="year" />
+        <Tooltip shared showCrosshairs />
+      </Chart>
+    );
+  }
+  if(num == 2){
+    return (
+      <Chart
+        scale={{ count: { min: 0, max: 25000 } }}
+        padding={[30, 0, 70, 40]}
+        autoFit
+        height={320}
+        data={data}
+        interactions={["element-active"]}
+      >
+        <Legend position={"top-left"}></Legend>
+        <Line shape="line" position="month*count" color="year" label={""} />
+        <Point position="month*count" color="year" />
+        <Tooltip shared showCrosshairs />
+      </Chart>
+    );
+  };
 };
+
 
 ReactDOM.render(LineChart, mountNode);
 
